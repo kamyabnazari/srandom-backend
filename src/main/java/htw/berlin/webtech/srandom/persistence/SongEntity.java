34d7afb94aa4211f133ef93ba356297d61d865a1
Kamyab Nazari <kamyab.nz@gmail.com>
2022-07-03 15:09:31 +0200

    small changes

--- src/main/java/htw/berlin/webtech/srandom/persistence/SongEntity.java
+++ src/main/java/htw/berlin/webtech/srandom/persistence/SongEntity.java
@@ -13,7 +13,7 @@ public class SongEntity {
     // numerischer Wert, sobald ein neuer Datensatz hinzugefügt wird → Strategy
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id", updatable = false, nullable = false)
     private long id;
 
     @Column(name = "title", nullable = false)
