3fb9663c260cd231eb749a38ce40fb7d5378dcdd
JonuziFlorina <86016416+JonuziFlorina@users.noreply.github.com>
2022-07-03 11:41:24 +0200

    SrandomApplicationTests added 5 more tests

--- src/test/java/htw/berlin/webtech/srandom/SrandomApplicationTests.java
+++ src/test/java/htw/berlin/webtech/srandom/SrandomApplicationTests.java
@@ -18,4 +18,49 @@ class SrandomApplicationTests {
 		String expectedTitle = "Sugar";
 		assertEquals(expectedTitle, actualTitle);
 	}
+
+	@Test
+	@DisplayName("get method of Song should give back the correct songLink")
+	void getSongLink() {
+		Song songObject = new Song(6, "Behute", "Senidah", 2021, "https://www.youtube.com/watch?v=Dz5zCSXI81Q", false, false);
+		String actualSongLink = songObject.getSongLink();
+		String expectedSongLink = "https://www.youtube.com/watch?v=Dz5zCSXI81Q";
+		assertEquals(expectedSongLink, actualSongLink);
+	}
+
+	@Test
+	@DisplayName("get method of Song should give back the correct author")
+	void getAuthor() {
+		Song songObject = new Song(15, "Let Me Love You", "Mario", 2004, "Link", false, false);
+		String actualAuthor = songObject.getAuthor();
+		String expectedAuthor = "Mario";
+		assertEquals(expectedAuthor, actualAuthor);
+	}
+
+	@Test
+	@DisplayName("get method of Song and give back the correct release year")
+	void get_correct_releaseYear() {
+		Song songObject = new Song(16, "Say My Name", "Destiny's child", 1999, "Link", false, false);
+		long actualReleaseYear = songObject.getReleaseYear();
+		long expectedReleaseYear = Long.parseLong("1999");
+		assertEquals(expectedReleaseYear, actualReleaseYear);
+	}
+
+	@Test
+	@DisplayName("get method of Song and give back the correct favorite song")
+	void get_correct_favorite_song() {
+		Song songObject = new Song(16, "Say My Name", "Destiny's child", 1999, "Link", false, false);
+		boolean actualIsFavorite = songObject.getIsFavorite();
+		boolean expectedIsFavorite = false;
+		assertEquals(expectedIsFavorite, actualIsFavorite);
+	}
+
+	@Test
+	@DisplayName("get method of Song and give back the correct original song")
+	void get_correct_original_song() {
+		Song songObject = new Song(16, "Say My Name", "Destiny's child", 1999, "Link", false, false);
+		boolean actualIsOriginal = songObject.getIsOriginal();
+		boolean expectedIsOriginal = false;
+		assertEquals(expectedIsOriginal, actualIsOriginal);
+	}
 }
